8ec0d12558297eb22266ca93db9061d681d116b8
Beatriz Navidad Vilches <beatriz@streamhpc.com>
2024-09-09 07:05:02 +0000

    Fixed tests from review comments
    - Simplified adjacent_find_impl functor definition
    - Added test for indirect_iterator

--- test/rocprim/test_device_adjacent_find.cpp
+++ test/rocprim/test_device_adjacent_find.cpp
@@ -22,6 +22,7 @@
 
 #include "../common_test_header.hpp"
 
+#include "indirect_iterator.hpp"
 #include "test_utils_custom_test_types.hpp"
 #include "test_utils_types.hpp"
 
@@ -37,84 +38,34 @@
 #include <numeric>
 #include <vector>
 
-// Adjacent find functors for half support
-template<class T>
-struct adjacent_find_impl
-{
-    template<class ForwardIt>
-    inline constexpr ForwardIt operator()(ForwardIt first, ForwardIt last)
-    {
-        return std::adjacent_find(first, last);
-    }
-
-    template<class ForwardIt, class BinaryPred>
-    inline constexpr ForwardIt operator()(ForwardIt first, ForwardIt last, BinaryPred p)
-    {
-        return std::adjacent_find(first, last, p);
-    }
-};
-
-template<>
-struct adjacent_find_impl<rocprim::half>
-{
-    template<class ForwardIt>
-    inline constexpr ForwardIt operator()(ForwardIt first, ForwardIt last)
-    {
-        if(first == last)
-            return last;
-
-        ForwardIt next = first;
-        ++next;
-
-        for(; next != last; ++next, ++first)
-            if(*first == *next)
-                return first;
-
-        return last;
-    }
-
-    template<class ForwardIt, class BinaryPred>
-    inline constexpr ForwardIt operator()(ForwardIt first, ForwardIt last, BinaryPred p)
-    {
-        if(first == last)
-            return last;
-
-        ForwardIt next = first;
-        ++next;
-
-        for(; next != last; ++next, ++first)
-            if(p(*first, *next))
-                return first;
-
-        return last;
-    }
-};
-
 // Params for tests
 template<class InputType,
-         class OutputType = std::size_t,
-         class OpType     = rocprim::equal_to<InputType>,
-         class Config     = rocprim::default_config,
-         bool UseGraphs   = false>
+         class OutputType         = std::size_t,
+         class OpType             = rocprim::equal_to<InputType>,
+         class Config             = rocprim::default_config,
+         bool UseGraphs           = false,
+         bool UseIndirectIterator = false>
 struct DeviceAdjacentFindParams
 {
-    using input_type                 = InputType;
-    using output_type                = OutputType;
-    using op_type                    = OpType;
-    using config                     = Config;
-    static constexpr bool use_graphs = UseGraphs;
+    using input_type                            = InputType;
+    using output_type                           = OutputType;
+    using op_type                               = OpType;
+    using config                                = Config;
+    static constexpr bool use_graphs            = UseGraphs;
+    static constexpr bool use_indirect_iterator = UseIndirectIterator;
 };
 
 template<class Params>
 class RocprimDeviceAdjacentFindTests : public ::testing::Test
 {
 public:
-    using input_type                        = typename Params::input_type;
-    using output_type                       = typename Params::output_type;
-    using op_type                           = typename Params::op_type;
-    using config                            = typename Params::config;
-    static constexpr bool use_graphs        = Params::use_graphs;
-    static constexpr bool debug_synchronous = false;
+    using input_type                            = typename Params::input_type;
+    using output_type                           = typename Params::output_type;
+    using op_type                               = typename Params::op_type;
+    using config                                = typename Params::config;
+    static constexpr bool use_graphs            = Params::use_graphs;
+    static constexpr bool use_indirect_iterator = Params::use_indirect_iterator;
+    static constexpr bool debug_synchronous     = false;
 };
 
 // Custom types
@@ -147,6 +98,13 @@ using RocprimDeviceAdjacentFindTestsParams = ::testing::Types<
                              std::size_t,
                              rocprim::equal_to<unsigned int>,
                              rocprim::default_config,
+                             true>,
+    // Tests for when output's value_type is void
+    DeviceAdjacentFindParams<int,
+                             std::size_t,
+                             rocprim::equal_to<int>,
+                             rocprim::default_config,
+                             false,
                              true>>;
 
 TYPED_TEST_SUITE(RocprimDeviceAdjacentFindTests, RocprimDeviceAdjacentFindTestsParams);
@@ -157,12 +115,14 @@ TYPED_TEST(RocprimDeviceAdjacentFindTests, AdjacentFind)
     SCOPED_TRACE(testing::Message() << "with device_id = " << device_id);
     HIP_CHECK(hipSetDevice(device_id));
 
-    using T                      = typename TestFixture::input_type;
-    using output_type            = typename TestFixture::output_type;
-    using op_type                = typename TestFixture::op_type;
-    const bool debug_synchronous = TestFixture::debug_synchronous;
-    using Config                 = typename TestFixture::config;
-    using adjacent_find          = adjacent_find_impl<T>;
+    using T                                     = typename TestFixture::input_type;
+    using output_type                           = typename TestFixture::output_type;
+    using op_type                               = typename TestFixture::op_type;
+    static constexpr bool use_indirect_iterator = TestFixture::use_indirect_iterator;
+    const bool            debug_synchronous     = TestFixture::debug_synchronous;
+    using Config                                = typename TestFixture::config;
+
+    op_type op{};
 
     for(std::size_t seed_index = 0; seed_index < random_seeds_count + seed_size; seed_index++)
     {
@@ -181,35 +141,23 @@ TYPED_TEST(RocprimDeviceAdjacentFindTests, AdjacentFind)
 
             SCOPED_TRACE(testing::Message() << "with size = " << size);
 
-            // Generate input values
-            std::vector<T> input;
-            if(rocprim::is_floating_point<T>::value)
-            {
-                input = test_utils::get_random_data<T>(size, -1000, 1000, seed_value);
-            }
-            else
-            {
-                input = test_utils::get_random_data<T>(size,
-                                                       test_utils::numeric_limits<T>::min(),
-                                                       test_utils::numeric_limits<T>::max(),
-                                                       seed_value);
-            }
-
             // Get random index for first adjacent pair
             std::size_t first_adj_index = 0;
             if(size > 1)
             {
                 first_adj_index
-                    = test_utils::get_random_value<std::size_t>(0, size - 2, seed_value);
+                    = std::min(test_utils::get_random_value<std::size_t>(
+                                   0,
+                                   static_cast<size_t>(test_utils::numeric_limits<T>::max()),
+                                   seed_value),
+                               size - 2);
             }
             SCOPED_TRACE(testing::Message() << "with first_adj_index = " << first_adj_index);
 
-            if(size > 1)
-            {
-                // Ensure that it is the real first adjacent pair index
-                std::iota(input.begin(), input.begin() + first_adj_index + 1, 0);
-                input[first_adj_index] = input[first_adj_index + 1];
-            }
+            // Generate input values
+            std::vector<T> input(size);
+            std::iota(input.begin(), input.begin() + first_adj_index, 0);
+            std::fill(input.begin(), input.end(), first_adj_index);
 
             T*           d_input;
             output_type* d_output;
@@ -221,15 +169,18 @@ TYPED_TEST(RocprimDeviceAdjacentFindTests, AdjacentFind)
                                 input.size() * sizeof(*d_input),
                                 hipMemcpyHostToDevice));
 
+            const auto output_it
+                = test_utils::wrap_in_identity_iterator<use_indirect_iterator>(d_output);
+
             // Allocate temporary storage
             std::size_t tmp_storage_size;
             void*       d_tmp_storage = nullptr;
             HIP_CHECK(::rocprim::adjacent_find<Config>(d_tmp_storage,
                                                        tmp_storage_size,
                                                        d_input,
-                                                       d_output,
+                                                       output_it,
                                                        size,
-                                                       op_type{},
+                                                       op,
                                                        stream,
                                                        debug_synchronous));
             ASSERT_GT(tmp_storage_size, 0);
@@ -245,9 +196,9 @@ TYPED_TEST(RocprimDeviceAdjacentFindTests, AdjacentFind)
             HIP_CHECK(::rocprim::adjacent_find<Config>(d_tmp_storage,
                                                        tmp_storage_size,
                                                        d_input,
-                                                       d_output,
+                                                       output_it,
                                                        size,
-                                                       op_type{},
+                                                       op,
                                                        stream,
                                                        debug_synchronous));
 
@@ -265,7 +216,9 @@ TYPED_TEST(RocprimDeviceAdjacentFindTests, AdjacentFind)
             HIP_CHECK(hipMemcpy(&output, d_output, sizeof(*d_output), hipMemcpyDeviceToHost));
 
             // Calculate expected results on host
-            const auto expected = adjacent_find()(input.cbegin(), input.cend()) - input.begin();
+            const auto expected
+                = (size > 1) ? std::adjacent_find(input.cbegin(), input.cend(), op) - input.begin()
+                             : size;
 
             // Check if output values are as expected
             ASSERT_EQ(output, expected);
