daa6ed10cc23e710a9ddf50030ffc29f41990213
Wayne Franz <wayfranz@amd.com>
2025-01-22 10:19:07 -0500

    Set default accumulator type for device_scan to output type
    Currently, we set the default accumulator type for device-wide scans
    to the type of the init value, or the input type. This can result in
    situations where the accumulator overflows if the init value type is
    smaller than the output type.
    
    This change modifies the default accumulator type, setting it to the
    output type.

--- rocprim/include/rocprim/device/device_scan.hpp
+++ rocprim/include/rocprim/device/device_scan.hpp
@@ -510,7 +510,7 @@ template<class Config = default_config,
          class OutputIterator,
          class BinaryFunction
          = ::rocprim::plus<typename std::iterator_traits<InputIterator>::value_type>,
-         class AccType = typename std::iterator_traits<InputIterator>::value_type>
+         class AccType = typename std::iterator_traits<OutputIterator>::value_type>
 inline hipError_t inclusive_scan(void*             temporary_storage,
                                  size_t&           storage_size,
                                  InputIterator     input,
@@ -551,7 +551,7 @@ template<class Config = default_config,
          class OutputIterator,
          class BinaryFunction
          = ::rocprim::plus<typename std::iterator_traits<InputIterator>::value_type>,
-         class AccType = typename std::iterator_traits<InputIterator>::value_type>
+         class AccType = typename std::iterator_traits<OutputIterator>::value_type>
 inline hipError_t deterministic_inclusive_scan(void*             temporary_storage,
                                                size_t&           storage_size,
                                                InputIterator     input,
@@ -676,7 +676,7 @@ template<class Config = default_config,
          class InitValueType,
          class BinaryFunction
          = ::rocprim::plus<typename std::iterator_traits<InputIterator>::value_type>,
-         class AccType = detail::input_type_t<InitValueType>>
+         class AccType = detail::input_type_t<typename std::iterator_traits<OutputIterator>::value_type>>
 inline hipError_t exclusive_scan(void*               temporary_storage,
                                  size_t&             storage_size,
                                  InputIterator       input,
@@ -718,7 +718,7 @@ template<class Config = default_config,
          class InitValueType,
          class BinaryFunction
          = ::rocprim::plus<typename std::iterator_traits<InputIterator>::value_type>,
-         class AccType = detail::input_type_t<InitValueType>>
+         class AccType = detail::input_type_t<typename std::iterator_traits<OutputIterator>::value_type>>
 inline hipError_t deterministic_exclusive_scan(void*               temporary_storage,
                                                size_t&             storage_size,
                                                InputIterator       input,
